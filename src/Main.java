989863f2d563b58384566ddbca6f75a96c11ed7c
Brshnrk18 <lr.bhmembreve@mmdc.mcl.edu.ph>
2026-03-13 19:42:57 +1100

    Test PartTimeStaff in Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
 import model.FullTimeStaff;
+import model.PartTimeStaff;
+import model.Employee;
 
 public class Main {
-
     public static void main(String[] args){
 
-        FullTimeStaff emp = new FullTimeStaff("John",1001,30000);
+        // Test Employee
+        Employee emp1 = new Employee("Alice",1001);
+        emp1.setHourlyRate(200);
+        emp1.setHoursWorked(8);
+        System.out.println(emp1.getName() + " Gross Pay: " + emp1.computeGross());
 
-        System.out.println("Employee: " + emp.getName());
-        System.out.println("Salary: " + emp.calculatePay());
+        // Test FullTimeStaff
+        FullTimeStaff emp2 = new FullTimeStaff("Bob",1002,30000);
+        System.out.println(emp2.getName() + " Monthly Salary: " + emp2.calculatePay());
 
+        // Test PartTimeStaff
+        PartTimeStaff emp3 = new PartTimeStaff("Charlie",1003,150);
+        double hoursWorked = 10;
+        System.out.println(emp3.getName() + " Part-Time Pay: " + emp3.calculatePay(hoursWorked));
     }
-
 }
-Added Main test file
