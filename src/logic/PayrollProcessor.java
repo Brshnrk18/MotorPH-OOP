c7d15eb6ed174e8e119af6a91342660f004a2f8a
Brshnrk18 <lr.bhmembreve@mmdc.mcl.edu.ph>
2026-03-19 21:19:59 +1100

    Enhance payroll report output format

--- src/logic/PayrollProcessor.java
+++ src/logic/PayrollProcessor.java
@@ -4,12 +4,11 @@ import model.Employee;
 import java.util.List;
 
 public class PayrollProcessor {
-
     public void runPayroll(List<Employee> staffList) {
-
-        for(Employee emp : staffList) {
+        System.out.println("--- MotorPH Payroll Report ---");
+        for (Employee emp : staffList) {
             double pay = emp.computeNetPay();
-            System.out.println(emp.getName() + " Net Pay: " + pay);
+            System.out.println("ID: " + emp.getName() + " | Net Pay: " + pay);
         }
     }
 }
