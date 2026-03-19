6d36fd45fc8f87453ef1b192ea502b9d28f35234
Brshnrk18 <lr.bhmembreve@mmdc.mcl.edu.ph>
2026-03-19 20:05:47 +1100

    Fixed FullTimeStaff

--- src/model/FullTimeStaff.java
+++ src/model/FullTimeStaff.java
@@ -4,29 +4,22 @@ public class FullTimeStaff extends Employee {
 
     private double salary;
 
-    public FullTimeStaff(String name, int id, double salary){
-        super(name,id);
+    public FullTimeStaff(String name, int id, double salary) {
+        super(name, id);
         this.salary = salary;
     }
 
-    public double calculatePay(){
-        return salary;
+    public double computeNetPay() {
+        double gross = salary + 500;
+
+        SSS sss = new SSS();
+        PhilHealth phil = new PhilHealth();
+        PagIBIG pag = new PagIBIG();
+
+        double total = sss.calculate(gross)
+                     + phil.calculate(gross)
+                     + pag.calculate(gross);
+
+        return gross - total;
     }
 }
-Added FullTimeStaff class
-@Override
-public double computeNetPay() {
-    double gross = calculatePay(); // full-time uses salary
-
-    // example allowance
-    gross += 500;
-
-    SSS sss = new SSS();
-    PhilHealth phil = new PhilHealth();
-    PagIBIG pag = new PagIBIG();
-
-    double sssAmount = sss.calculate(gross);
-    double philAmount = phil.calculate(gross);
-    double pagAmount = pag.calculate(gross);
-
-    return gross - (sssAmount + philAmount + pagAmount);
