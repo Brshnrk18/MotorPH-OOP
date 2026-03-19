6b5a01dce97509a1ee78ffa383993169d3def9cb
Brshnrk18 <lr.bhmembreve@mmdc.mcl.edu.ph>
2026-03-19 21:18:27 +1100

    Simplify PartTimeStaff by removing method override
    Removed overridden computeNetPay method in PartTimeStaff class.

--- src/model/PartTimeStaff.java
+++ src/model/PartTimeStaff.java
@@ -1,15 +1,10 @@
 package model;
 
 public class PartTimeStaff extends Employee {
-
     public PartTimeStaff(String name, int id, double rate, double hours) {
         super(name, id);
         setHourlyRate(rate);
         setHoursWorked(hours);
     }
-
-    @Override
-    public double computeNetPay() {
-        return super.computeNetPay();
-    }
+    // Uses the parent's computeNetPay logic
 }
