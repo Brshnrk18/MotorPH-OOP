55f0c0d8b28f9a23e53bb5cc46971c0db1cfabcd
Brshnrk18 <lr.bhmembreve@mmdc.mcl.edu.ph>
2026-03-19 20:06:16 +1100

    Fixed PartTimeStaff
    Updated PartTimeStaff constructor and removed overridden computeNetPay method.

--- src/model/PartTimeStaff.java
+++ src/model/PartTimeStaff.java
@@ -2,29 +2,14 @@ package model;
 
 public class PartTimeStaff extends Employee {
 
-    private double hourlyRate;
-
-    public PartTimeStaff(String name, int id, double hourlyRate) {
+    public PartTimeStaff(String name, int id, double rate, double hours) {
         super(name, id);
-        this.hourlyRate = hourlyRate;
+        setHourlyRate(rate);
+        setHoursWorked(hours);
     }
 
-    // Override calculatePay() for part-time employees
-    public double calculatePay(double hoursWorked) {
-        return hourlyRate * hoursWorked;
+    @Override
+    public double computeNetPay() {
+        return super.computeNetPay();
     }
-}
-@Override
-public double computeNetPay() {
-    double gross = computeGross(); // hourly * hours
-
-    SSS sss = new SSS();
-    PhilHealth phil = new PhilHealth();
-    PagIBIG pag = new PagIBIG();
-
-    double sssAmount = sss.calculate(gross);
-    double philAmount = phil.calculate(gross);
-    double pagAmount = pag.calculate(gross);
-
-    return gross - (sssAmount + philAmount + pagAmount);
 }
