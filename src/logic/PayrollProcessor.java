2d2062d90961864569d53d4b66ee2fc7988c4fab
Brshnrk18 <lr.bhmembreve@mmdc.mcl.edu.ph>
2026-03-19 20:06:43 +1100

    Refactored PayrollProcessor (polymorphism)
    Removed commented-out code and adjusted output message.

--- src/logic/PayrollProcessor.java
+++ src/logic/PayrollProcessor.java
@@ -1,27 +1,15 @@
 package logic;
 
 import model.Employee;
-import model.FullTimeStaff;
-import model.PartTimeStaff;
 import java.util.List;
 
 public class PayrollProcessor {
 
-    // Run payroll for a list of employees
     public void runPayroll(List<Employee> staffList) {
 
-        // Loop through all employees
         for(Employee emp : staffList) {
-
-            // Polymorphism: calls the correct computeNetPay()
             double pay = emp.computeNetPay();
-
-            // Display result
             System.out.println(emp.getName() + " Net Pay: " + pay);
-        }
-    }
-}
-            System.out.println(emp.getName() + " Pay: " + pay);
         }
     }
 }
