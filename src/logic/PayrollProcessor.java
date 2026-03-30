afe22e6e8fdf6cf5e767f32f1154dd43bbe34d8f
Brshnrk18 <lr.bhmembreve@mmdc.mcl.edu.ph>
2026-03-30 20:42:20 +1100

    Update PayrollProcessor.java

--- src/logic/PayrollProcessor.java
+++ src/logic/PayrollProcessor.java
@@ -1,14 +1,23 @@
 package logic;
 
 import model.Employee;
-import java.util.List;
 
 public class PayrollProcessor {
-    public void runPayroll(List<Employee> staffList) {
-        System.out.println("--- MotorPH Payroll Report ---");
-        for (Employee emp : staffList) {
-            double pay = emp.computeNetPay();
-            System.out.println("ID: " + emp.getName() + " | Net Pay: " + pay);
-        }
+
+    public double calculateNetValue(Employee emp) {
+        double gross = emp.computeGross();
+        double sss = emp.calculateSSS();
+        double phil = emp.calculatePhilHealth();
+        double pag = emp.calculatePagIBIG();
+        double net = emp.computeNetPay();
+
+        System.out.println("\n--- QA Internal Trace ---");
+        System.out.println("Employee: " + emp.getName() + " (" + emp.getClass().getSimpleName() + ")");
+        System.out.printf("Gross: %.2f | SSS: %.2f | PhilHealth: %.2f | Pag-IBIG: %.2f\n", 
+                          gross, sss, phil, pag);
+        System.out.printf("Calculated Net: %.2f\n", net);
+        System.out.println("--------------------------");
+
+        return net;
     }
 }
